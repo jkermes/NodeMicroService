7b9093baf2c5b6cc5d24bc05746b12168e5553a0
Pierre CHARLES <pier.charles@hotmail.fr>
2016-02-14 13:03:13 +0100

    Add table list component

--- front/js/main.js
+++ front/js/main.js
@@ -3,24 +3,60 @@ import { render } from 'react-dom'
 
 var serverUrl = 'http://localhost:9312';
 
-fetch(serverUrl, {
-    method: 'get'
-}).then(function(response) {
-    return response.json();
-}).then(function(data) {
-    console.log('data' + data);
-}).catch(function(error) {
-    console.log('error :' + error);
-});
-
 var Episode = React.createClass({
     render: function() {
-        return (
+        return (<div>
+			<EpisodeList />
             <EpisodeForm />
+            </div>
         );
     }
 });
 
+var EpisodeList = React.createClass({
+	getInitialState: function() {
+        return {episodes: ''};
+    },
+	loadAllEpisodes: function() {
+		fetch(serverUrl, {
+			method: 'get'
+		}).then(function(response) {
+			return response.json();
+		}).then(function(data) {
+			this.setState({episodes: data});
+		}).catch(function(error) {
+			console.log('error :' + error);
+		});
+	},
+	render: function(){
+		return (
+			
+			<tr>
+				<td>Title</td>
+				<td>Saison</td>
+				<td>Episode</td>
+			</tr>
+			
+		);
+	},
+	componentWillMount: function() {
+		this.loadAllEpisodes();
+		console.log(this.state.episodes);
+	}
+});
+
+var EpisodeListItem = React.createClass({
+	render: function(){
+		return (
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			);
+	}
+});
+	
 var EpisodeForm = React.createClass({
     getInitialState: function() {
         return {title: '', season: '', episode: ''};
